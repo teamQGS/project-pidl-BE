a64ef2589912a0fa7c71c4829e668e953f5f3829
Sawna <smakhnovskiy19@gmail.com>
2024-03-24 22:21:06 +0100

    Added registration and login logic + initial logic of tokens.

--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -1,41 +1,78 @@
 package com.example.demo.Service;
 
+import com.example.demo.DTOS.LoginDTO;
+import com.example.demo.DTOS.SignUpDTO;
 import com.example.demo.DTOS.UserDTO;
+import com.example.demo.exceptions.AppException;
+import com.example.demo.mappers.UserMapper;
 import com.example.demo.model.Entities.UserEntity;
 import com.example.demo.model.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.nio.CharBuffer;
 import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    UserMapper userMapper;
 
-    public Long createUser(UserDTO userDTO){
-        UserEntity entity = new UserEntity();
-        entity.setUsername(userDTO.getUsername());
-        entity.setEmail(userDTO.getEmail());
-        entity.setPassword(userDTO.getPassword());
-        userRepository.save(entity);
-        return entity.getId();
-    }
+    public UserDTO login(LoginDTO loginDTO){
+        UserEntity user = userRepository.findByEmail(loginDTO.email())
+                .orElseThrow(() -> new AppException("Unknown User", HttpStatus.NOT_FOUND));
 
-    public UserDTO deleteUser(Long id){
-        Optional<UserEntity> optEntity = userRepository.findById(id);
-        if (optEntity.isEmpty()) {
-            System.out.println("There is no book with id: " + id);
+        if (passwordEncoder.matches(CharBuffer.wrap(loginDTO.password()), user.getPassword())) {
+            System.out.println("login successful " + loginDTO.email());
+            return userMapper.toUserDTO(user);
         }
-        UserEntity userEntity = optEntity.get();
-        userRepository.deleteById(id);
-        System.out.println("Book with id: " + id + " was deleted!");
-
-        UserDTO dto = new UserDTO();
-        dto.setId(userEntity.getId());
-        dto.setUsername(userEntity.getUsername());
-        dto.setEmail(userEntity.getEmail());
-        dto.setPassword(userEntity.getPassword());
-        return dto;
+        throw new AppException("Invalid password", HttpStatus.BAD_REQUEST);
     }
+
+    public UserDTO register(SignUpDTO signUpDTO){
+        Optional<UserEntity> user = userRepository.findByEmail(signUpDTO.email());
+
+        if(user.isPresent()) {
+            throw new AppException("Email exists", HttpStatus.BAD_REQUEST);
+        }
+        UserEntity userEntity = userMapper.signUpToUser(signUpDTO);
+        userEntity.setPassword(passwordEncoder.encode(CharBuffer.wrap(signUpDTO.password())));
+        UserEntity saved = userRepository.save(userEntity);
+        return userMapper.toUserDTO(saved);
+    }
+
+
+
+
+    //    public Long createUser(UserDTO userDTO){
+//        UserEntity entity = new UserEntity();
+//        entity.setUsername(userDTO.getUsername());
+//        entity.setEmail(userDTO.getEmail());
+//        entity.setPassword(userDTO.getPassword());
+//        userRepository.save(entity);
+//        return entity.getId();
+//    }
+
+
+//    public UserDTO deleteUser(Long id){
+//        Optional<UserEntity> optEntity = userRepository.findById(id);
+//        if (optEntity.isEmpty()) {
+//            System.out.println("There is no book with id: " + id);
+//        }
+//        UserEntity userEntity = optEntity.get();
+//        userRepository.deleteById(id);
+//        System.out.println("Book with id: " + id + " was deleted!");
+//
+//        UserDTO dto = new UserDTO();
+//        dto.setId(userEntity.getId());
+//        dto.setUsername(userEntity.getUsername());
+//        dto.setEmail(userEntity.getEmail());
+//        return dto;
+//    }
 }
--- src/main/java/com/example/demo/model/Repositories/UserRepository.java
+++ src/main/java/com/example/demo/model/Repositories/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Entities.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
