e2ab17c2547ea8ec9077fd8e7b116dd85b3a8404
quadrk <quadrk@pm.me>
2024-03-26 16:10:09 +0100

    Add slf4j-api dependency and update logging in UserController

--- src/main/java/com/team992/pidl/Controllers/UserController.java
+++ src/main/java/com/team992/pidl/Controllers/UserController.java
@@ -3,26 +3,23 @@ package com.team992.pidl.Controllers;
 import com.team992.pidl.DTOS.UserDTO;
 import com.team992.pidl.Service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/users")
-@AllArgsConstructor //for userService
+@AllArgsConstructor // for userService
 public class UserController {
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @PostMapping
     public Long registerUser(@RequestBody UserDTO userDTO) {
-        System.out.println("Registering user " + userDTO);
+        logger.info("Registering user {}", userDTO);
         Long info = userService.createUser(userDTO);
-        System.out.println("User was created with ID: " + info);
+        logger.info("User was created with ID: {}", info);
         return info;
     }
 
