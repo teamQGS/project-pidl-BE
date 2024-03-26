a1f0a22a919812c4e4cb82612cd73db20f3af7ad
quadrk <quadrk@pm.me>
2024-03-26 16:02:01 +0100

    Add Spring Boot web dependency and update UserController

--- src/main/java/com/team992/pidl/Controllers/UserController.java
+++ src/main/java/com/team992/pidl/Controllers/UserController.java
@@ -3,16 +3,19 @@ package com.team992.pidl.Controllers;
 import com.team992.pidl.DTOS.UserDTO;
 import com.team992.pidl.Service.UserService;
 import lombok.AllArgsConstructor;
-import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/users")
 @AllArgsConstructor //for userService
 public class UserController {
-    @Autowired
     private final UserService userService;
 
     @PostMapping
