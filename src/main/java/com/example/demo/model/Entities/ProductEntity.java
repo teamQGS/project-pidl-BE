43c27a5f81149625f4b8fa3be8909e15628d36c6
good <good@quadrk.space>
2024-03-27 20:40:46 +0100

    Added basic controllers and repositories. Started working on user logic

--- src/main/java/com/example/demo/model/Entities/ProductEntity.java
+++ src/main/java/com/example/demo/model/Entities/ProductEntity.java
@@ -7,7 +7,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.awt.image.BufferedImage;
 
 
 @Data
--- src/main/java/com/example/demo/model/Entities/WarehouseEntity.java
+++ src/main/java/com/example/demo/model/Entities/WarehouseEntity.java
@@ -7,9 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
