68f42fdc19ae6e7818c9346b868a701935d2a512
quadrk <quadrk@pm.me>
2024-04-08 12:55:36 +0200

    Refactor user deletion methods to use email instead of ID

--- src/main/java/com/team992/pidl/Service/UserService.java
+++ src/main/java/com/team992/pidl/Service/UserService.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
-
 import java.util.NoSuchElementException;
 
 @Service
@@ -38,26 +36,45 @@ public class UserService {
         return id;
     }
 
-    public UserDTO deleteUser(String id){
-        logger.info("Deleting user with ID: {}", id);
-        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No user found with id: " + id));
+    public UserDTO deleteUserByEmail(String email){
+        logger.info("Deleting user with Email: {}", email);
+        UserEntity userEntity = userRepository.findByContact_Email(email).orElseThrow(() -> new NoSuchElementException("No user found with email: " + email));
         UserDTO dto = toUserDTO(userEntity);
         contactRepository.delete(userEntity.getContact());
         logger.info("Deleted contact with email: {}", userEntity.getContact().getEmail());
-        userRepository.deleteById(id);
-        logger.info("User with id: {} was deleted.", id);
+        userRepository.delete(userEntity);
+        logger.info("User with Email: {} was deleted.", email);
         return dto;
     }
 
-    public UserDTO getUser(String id){
-        logger.info("Retrieving user with ID: {}", id);
-        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No user found with id: " + id));
+    // Old method to delete user by id, kept for reference
+    // public UserDTO deleteUser(String id){
+    //     logger.info("Deleting user with ID: {}", id);
+    //     UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No user found with id: " + id));
+    //     UserDTO dto = toUserDTO(userEntity);
+    //     contactRepository.delete(userEntity.getContact());
+    //     logger.info("Deleted contact with email: {}", userEntity.getContact().getEmail());
+    //     userRepository.deleteById(id);
+    //     logger.info("User with id: {} was deleted.", id);
+    //     return dto;
+    // }
+
+    public UserDTO getUserByEmail(String email){
+        logger.info("Retrieving user with Email: {}", email);
+        UserEntity userEntity = userRepository.findByContact_Email(email).orElseThrow(() -> new NoSuchElementException("No user found with email: " + email));
         return toUserDTO(userEntity);
     }
 
-    public UserDTO updateUser(String id, UserDTO userDTO){
-        logger.info("Updating user with ID: {}", id);
-        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No user found with id: " + id));
+    // Old method to get user by id, kept for reference
+    // public UserDTO getUser(String id){
+    //     logger.info("Retrieving user with ID: {}", id);
+    //     UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No user found with id: " + id));
+    //     return toUserDTO(userEntity);
+    // }
+
+    public UserDTO updateUserByEmail(String email, UserDTO userDTO){
+        logger.info("Updating user with Email: {}", email);
+        UserEntity userEntity = userRepository.findByContact_Email(email).orElseThrow(() -> new NoSuchElementException("No user found with email: " + email));
         userEntity.setUsername(userDTO.getUsername());
         userEntity.setPassword(userDTO.getPassword());
         ContactEntity contactEntity = userEntity.getContact();
@@ -67,9 +84,26 @@ public class UserService {
             contactRepository.save(contactEntity);
         }
         userRepository.save(userEntity);
-        logger.info("User with ID: {} was updated.", id);
+        logger.info("User with Email: {} was updated.", email);
         return toUserDTO(userEntity);
     }
+
+    // Old method to update user by id, kept for reference
+    // public UserDTO updateUser(String id, UserDTO userDTO){
+    //     logger.info("Updating user with ID: {}", id);
+    //     UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("No user found with id: " + id));
+    //     userEntity.setUsername(userDTO.getUsername());
+    //     userEntity.setPassword(userDTO.getPassword());
+    //     ContactEntity contactEntity = userEntity.getContact();
+    //     if (!contactEntity.getEmail().equals(userDTO.getEmail())) {
+    //         logger.info("Updating contact email from {} to {}", contactEntity.getEmail(), userDTO.getEmail());
+    //         contactEntity.setEmail(userDTO.getEmail());
+    //         contactRepository.save(contactEntity);
+    //     }
+    //     userRepository.save(userEntity);
+    //     logger.info("User with ID: {} was updated.", id);
+    //     return toUserDTO(userEntity);
+    // }
 
     public List<UserDTO> getAllUsers() {
         logger.info("Retrieving all users");
