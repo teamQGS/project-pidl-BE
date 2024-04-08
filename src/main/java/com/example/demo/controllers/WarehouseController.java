f12b25bb336f5b4229082e85e6baf0025eb5efbb
good <good@quadrk.space>
2024-04-08 13:34:12 +0200

    added controller logic for warehouse

--- src/main/java/com/example/demo/controllers/WarehouseController.java
+++ src/main/java/com/example/demo/controllers/WarehouseController.java
@@ -1,14 +1,34 @@
 package com.example.demo.controllers;
 
+import com.example.demo.DTOS.WarehouseDTO;
+import com.example.demo.model.Entities.WarehouseEntity;
+import com.example.demo.services.WarehouseService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/api/warehouse")
 public class WarehouseController {
-//    @GetMapping
-//    public ResponseEntity<List<WarehouseEntity>> getWarehouseInfo(){
-//         //TODO warehouse logic return is for not getting an error
-//    }
+
+    @Autowired
+    WarehouseService service;
+    @GetMapping
+    public ResponseEntity<List<WarehouseDTO>> getWarehouseInfo(){
+         return new ResponseEntity<>(service.getAllWarehouses(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<WarehouseDTO>> getWarehouseInfoById(@PathVariable ObjectId id){
+        return new ResponseEntity<>(service.getWarehouseById(id), HttpStatus.OK);
+    }
 }
