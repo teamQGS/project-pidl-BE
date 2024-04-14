da2a98147c2160787b93ced78640a95423dd2339
quadrk <quadrk@pm.me>
2024-04-15 00:37:58 +0200

    Refactor TestEntities class and add OrderRepositoryTest class

---
+++ src/test/java/com/example/demo/Entities/TestEntities.java
@@ -0,0 +1,78 @@
+package com.example.demo.Entities;
+
+import com.example.demo.model.Entities.*;
+import com.example.demo.model.Entities.Enums.Status;
+import org.bson.types.ObjectId;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+
+// This class is used to create entities for testing purposes
+public class TestEntities {
+
+    public static UserEntity createUser() {
+        UserEntity user = new UserEntity();
+        user.setId(new ObjectId());
+        user.setUsername("testUser");
+        user.setPassword("testPassword");
+        user.setRoles(new HashSet<>());
+        user.setFirstName("testFirstName");
+        user.setLastName("testLastName");
+        user.setToken("testToken");
+        user.setAge(30);
+        user.setIsAdult(true);
+        user.setImage("testImage");
+        return user;
+    }
+
+    public static ProductEntity createProduct() {
+        ProductEntity product = new ProductEntity();
+        product.setId(new ObjectId());
+        product.setName("testProductName");
+        product.setDescription("testDescription");
+        product.setPrice(100);
+        product.setIllustration("testIllustration");
+        product.setWarehouse(createWarehouse());
+        return product;
+    }
+
+    public static WarehouseEntity createWarehouse() {
+        WarehouseEntity warehouse = new WarehouseEntity();
+        warehouse.setId(new ObjectId());
+        warehouse.setCount(100);
+        warehouse.setTotalByes(1000);
+        warehouse.setProductId(new ObjectId());
+        return warehouse;
+    }
+
+    public static AddressEntity createAddress() {
+        AddressEntity address = new AddressEntity();
+        address.setId(new ObjectId());
+        address.setUserId(createUser());
+        address.setLocation("testLocation");
+        return address;
+    }
+
+    public static OrderEntity createOrder() {
+        OrderEntity order = new OrderEntity();
+        order.setId(new ObjectId());
+        order.setDate(new Date());
+        order.setUserId(createUser());
+        order.setProductIds(Arrays.asList(createProduct()));
+        order.setCount(Arrays.asList(1)); // Здесь мы предполагаем, что у вас есть один продукт каждого типа в заказе
+        order.setTotalSum(100);
+        order.setAddressId(createAddress());
+        order.setStatus(Status.PAID);
+        return order;
+    }
+
+    public static ContactEntity createContact() {
+        ContactEntity contact = new ContactEntity();
+        contact.setId(new ObjectId());
+        contact.setUserId(createUser());
+        contact.setEmail("test@example.com");
+        contact.setPhone("1234567890");
+        return contact;
+    }
+}
---
+++ src/test/java/com/example/demo/OrderRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import com.example.demo.model.Entities.OrderEntity;
+import com.example.demo.model.Repositories.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import com.example.demo.Entities.TestEntities;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// TODO: Fix org.springframework.dao.DataIntegrityViolationException caused by the old staff fields in database
+@DataMongoTest
+public class OrderRepositoryTest {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Test
+    public void testSaveOrder() {
+        OrderEntity order = TestEntities.createOrder();
+
+        orderRepository.save(order);
+
+        OrderEntity foundOrder = orderRepository.findById(order.getId()).orElse(null);
+        assertThat(foundOrder).isNotNull();
+        assertThat(foundOrder).isEqualToComparingFieldByField(order);
+    }
+
+    @Test
+    public void testFindOrderById() {
+        OrderEntity order = TestEntities.createOrder();
+
+        orderRepository.save(order);
+
+        Optional<OrderEntity> foundOrder = orderRepository.findById(order.getId());
+        assertThat(foundOrder).isPresent();
+        assertThat(foundOrder.get()).isEqualToComparingFieldByField(order);
+    }
+
+    @Test
+    public void testUpdateOrder() {
+        OrderEntity order = TestEntities.createOrder();
+
+        orderRepository.save(order);
+
+        order.setTotalSum(200);
+        orderRepository.save(order);
+
+        OrderEntity foundOrder = orderRepository.findById(order.getId()).orElse(null);
+        assertThat(foundOrder).isNotNull();
+        assertThat(foundOrder.getTotalSum()).isEqualTo(200);
+    }
+
+    @Test
+    public void testDeleteOrder() {
+        OrderEntity order = TestEntities.createOrder();
+
+        orderRepository.save(order);
+        orderRepository.delete(order);
+
+        OrderEntity foundOrder = orderRepository.findById(order.getId()).orElse(null);
+        assertThat(foundOrder).isNull();
+    }
+}
