063ee1fe0babb2929cdc079c82c2373ee8c037b8
quadrk <quadrk@pm.me>
2024-04-14 16:18:58 +0200

    Add new roles to Role enum

--- src/main/java/com/example/demo/model/Entities/Enums/Role.java
+++ src/main/java/com/example/demo/model/Entities/Enums/Role.java
@@ -1,7 +1,8 @@
 package com.example.demo.model.Entities.Enums;
 
 public enum Role {
-    //TODO add roles
     ADMIN,
-    OTHER //TODO add the rest of the roles
+    MANAGER,
+    CUSTOMER,
+    GUEST
 }
